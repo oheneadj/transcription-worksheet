211531908f9d1f5ff07a75f5d5b7155131a3efc5
oheneadj <oheneadjei.dev@gmail.com>
2022-10-26 10:24:04 +0000

    added function for grab and pluck

--- dist/js/app.js
+++ dist/js/app.js
@@ -21,24 +21,86 @@ const streamAlert = document.getElementById("stream-alert");
 const wentAlert = document.getElementById("went-alert");
 const rougeAlert = document.getElementById("rouge-alert");
 
-const checkAnswer = () => {
-  console.log(grabSelect.value);
-
-  if (grabSelect.value == "[ˈgɹæb]") {
-    grabAlert.classList.remove("d-none");
-    grabAlert.classList.remove("alert-danger");
-    grabAlert.classList.add("alert-success");
-    grabAlert.innerText = "This is corect";
-  } else {
-    grabAlert.classList.remove("d-none");
-    grabAlert.classList.remove("alert-success");
-    grabAlert.classList.add("alert-danger");
-    grabAlert.innerText = "This is wrong";
+//Funtions to check answers
+// Grab
+function checkGrabAnswer(alert, select) {
+  if (select.value == "[ˈgɹæb]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-danger");
+    alert.classList.add("alert-success");
+    alert.innerText = "This is corect";
+  } else if (select.value == "[ˈgræb]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText =
+      "Incorrect. The English rhotic approximant is represented by turned r.";
+  } else if (select.value == "[ˈgɹaeb]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText =
+      "Incorrect. In IPA, any sequence of vowels indicates a diphthong.";
+  } else if (select.value == "[ˈkɹæb]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText = "Incorrect. That is crab not grab.";
   }
-};
+}
 
-grabBtn.addEventListener("click", checkAnswer);
+// Pluck
+function checkPluckAnswer(alert, select) {
+  if (select.value == "[ˈpluk]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-danger");
+    alert.classList.add("alert-success");
+    alert.innerText = "This is corect";
+  } else if (select.value == "[ˈplək]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText =
+      "Incorrect. The English rhotic approximant is represented by turned r.";
+  } else if (select.value == "[ˈplʌk]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText =
+      "Incorrect. In IPA, any sequence of vowels indicates a diphthong.";
+  } else if (select.value == "[ˈplʌck]") {
+    alert.classList.remove("d-none");
+    alert.classList.remove("alert-success");
+    alert.classList.add("alert-danger");
+    alert.innerText = "Incorrect. That is pluck not lsla.";
+  }
+}
 
-const words = {
-  name: "grab",
-};
+// Event Listeners
+grabBtn.addEventListener("click", () => {
+  checkGrabAnswer(grabAlert, grabSelect);
+});
+
+pluckBtn.addEventListener("click", () => {
+  checkPluckAnswer(pluckAlert, pluckSelect);
+});
+
+let word = [
+  {
+    name: "[ˈgɹæb]",
+    prompt: "This is correct",
+  },
+  {
+    name: "[ˈgræb]",
+    prompt:
+      "Incorrect. The English rhotic approximant is represented by turned r.",
+  },
+  {
+    name: "[ˈgɹaeb]",
+    prompt: "Incorrect. In IPA, any sequence of vowels indicates a diphthong.",
+  },
+  {
+    name: "[ˈkɹæb]",
+    prompt: "Incorrect. That is crab not grab.",
+  },
+];
